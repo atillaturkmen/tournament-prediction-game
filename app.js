dc78ad7d518a334d644d377799357b9f1caf6f7b
atillaturkmen <turkmenatilla522@gmail.com>
2021-06-24 22:24:24 +0300

    add 404 page

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express(); // create express app
 
 app.set("view engine", "ejs"); // use ejs as view engine
 
-app.use('/public', express.static(path.join(__dirname ,'public'))); // declare public folder
+app.use('/public', express.static(path.join(__dirname, 'public'))); // declare public folder
 
 // Start listening to given port
 const server = app.listen(80, () => {
@@ -17,3 +17,23 @@ const server = app.listen(80, () => {
 // Let routes folder handle every get and post request
 app.get("*", routes);
 app.post("*", routes);
+
+// If routes above could not respond, then page does not exist
+app.use(function (req, res, next) {
+    res.status(404);
+
+    // respond with html page
+    if (req.accepts('html')) {
+        res.render('404', { url: req.url });
+        return;
+    }
+
+    // respond with json
+    if (req.accepts('json')) {
+        res.json({ error: 'Not found' });
+        return;
+    }
+
+    // default to plain-text. send()
+    res.type('txt').send('Not found');
+});
