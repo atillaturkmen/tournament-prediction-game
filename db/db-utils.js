39fb19429426a1c8cfdc27ee414ce28c2ff32a3d
atillaturkmen <turkmenatilla522@gmail.com>
2021-06-28 21:59:42 +0300

    added winner and score guesses to homepage

--- db/db-utils.js
+++ db/db-utils.js
@@ -19,7 +19,7 @@ exports.createUser = function (username, pass) {
 
 // Returns users by descending score, max 10 people
 exports.getTopTen = function () {
-    return query("SELECT username, points FROM user ORDER BY points DESC LIMIT 10;");
+    return query("SELECT username, points, correct_winner_guesses, correct_score_guesses FROM user ORDER BY points DESC LIMIT 10;");
 };
 
 // Returns point of a user by username
