c78cb39f58876ffe35264beca2f89a4315135a65
atillaturkmen <turkmenatilla522@gmail.com>
2021-07-03 15:09:19 +0300

    fix date display, create partials folder

--- helper.js
+++ helper.js
@@ -6,3 +6,15 @@ exports.capitalizeTheFirstLetterOfEachWord = function (words) {
     }
     return separateWord.join(' ');
 };
+
+exports.changeDateDisplayOfMatches = function (matches) {
+    for (let i = 0; i < matches.length; i++) {
+        let date = matches[i].time.split(" ")[0];
+        let time = matches[i].time.split(" ")[1];
+        date = date.split("-").reverse().join(".");
+        date = date.slice(0, 5) + "."  + date.slice(8);
+        time = time.replace(":", ".");
+        matches[i].date = date;
+        matches[i].hour = time;
+    }
+};
